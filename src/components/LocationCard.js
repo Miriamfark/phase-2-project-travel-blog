0d80fcecad4892aa8d087f7e5e6b292c24ae2abb
Miriam <miriamjoshua00@gmail.com>
2022-07-13 21:08:54 +0300

    styled button

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -13,7 +13,7 @@ const LocationCard = ({ food, landmark, city, country, setPassport, passport, lo
   return (
     <div>
         <p>{city} is located in {country}. It is known for its delicious {food}. Make sure to visit the {landmark}!</p>
-        <button onClick={()=>handleClick(location)}>I visited here!</button>
+        <button className="button" onClick={()=>handleClick(location)}>I visited here!</button>
     </div>
   )
 }
